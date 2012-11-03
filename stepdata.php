9f44a96bc202a6a0953c2d3d27d1a1787500b218
Jordan Grant <jordangrantf15e@yahoo.com>
2012-11-03 10:59:00 -0700

    update2

--- stepdata.php
+++ stepdata.php
@@ -105,37 +105,6 @@ if(isset($_POST['section'])) {
 
 
 	
-	
-	/* create one master array of the records */
-/*	$posts = array();
-//	if(mysql_num_rows($result)) {
-//		$post = mysql_fetch_assoc($result);
-//	}
-
-	/* output in necessary format */
-//	if($format == 'json') {
-//		header('Content-type: application/json');
-//		echo json_encode($post);
-//	}
-//	else {
-//		header('Content-type: text/xml');
-//		echo '<posts>';
-//		foreach($posts as $index => $post) {
-//			if(is_array($post)) {
-//				foreach($post as $key => $value) {
-//					echo '<',$key,'>';
-//					if(is_array($value)) {
-//						foreach($value as $tag => $val) {
-//							echo '<',$tag,'>',htmlentities($val),'</',$tag,'>';
-//						}
-//					}
-//					echo '</',$key,'>';
-//				}
-//			}
-//		}
-//		echo '</posts>';
-//	}
-
 	/* disconnect from the db */
 	@mysql_close($link);
 }
